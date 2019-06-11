90f514ee8c3a9e5d2932488ba7a24767d6d45f10
Jhymer Martínez <jmartinez@ioet.com>
2019-06-11 14:36:38 -0500

    Rename project

--- ios/YbMusicAppTests/YbMusicAppTests.m
+++ ios/YbMusicAppTests/YbMusicAppTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface YbMusicAppTests : XCTestCase
+@interface ybmusicappTests : XCTestCase
 
 @end
 
-@implementation YbMusicAppTests
+@implementation ybmusicappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
